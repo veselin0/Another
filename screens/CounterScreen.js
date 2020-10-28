1af0f19e3f99e2f10bd556c69691b6aa4ad66b53
Veselin <vesselin60@gmail.com>
2020-10-28 16:09:27 +0200

    in counter demo manage state with useReducer hook

--- screens/CounterScreen.js
+++ screens/CounterScreen.js
@@ -1,32 +1,33 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const reducer = (state, action) => {
+  // state === { count: number Ç
+  // action === { typa: 'icremenmt' || 'decrement', payload: 1 }
+
+  switch (action.type) {
+    case 'increment':
+      return { ...state, count: state.count + action.payload };
+    case 'decrement':
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
+
 export default function CounterScreen() {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
 
   return (
     <View>
       <View style={styles.space}>
-        <Button
-          title="Increase"
-          onPress={() => {
-            // don't do this!
-            //counter++;
-            setCounter(counter + 1);
-            console.log(counter);
-          }}
-        />
+        <Button title="Increase" onPress={() => dispatch({ type: 'increment', payload: 1 })} />
       </View>
       <View style={styles.space}>
-        <Button
-          title="decrease"
-          onPress={() => {
-            setCounter(counter - 1);
-          }}
-        />
+        <Button title="decrease" onPress={() => dispatch({ type: 'decrement', payload: 1 })} />
       </View>
       <View style={styles.space}>
-        <Text style={styles.text}>Current Count:{counter}</Text>
+        <Text style={styles.text}>Current Count: {state.count}</Text>
       </View>
     </View>
   );
