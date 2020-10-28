818571588ea5cdf58681f60aab83bb5bcb8d18e0
Veselin <vesselin60@gmail.com>
2020-10-28 16:35:54 +0200

    63. Two Important Props

--- screens/TextScreen.js
+++ screens/TextScreen.js
@@ -4,7 +4,11 @@ import { View, TextInput, StyleSheet, Text } from 'react-native';
 export default function TextScreen() {
   return (
     <View>
-      <TextInput style={styles.input} />
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
     </View>
   );
 }
