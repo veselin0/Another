2ff3d18cb599c23f3c0cab0957c7bd94a4c1a0c8
Veselin <vesselin60@gmail.com>
2020-10-29 10:44:04 +0200

    75. The Position Property

--- screens/BoxScreen.js
+++ screens/BoxScreen.js
@@ -16,7 +16,8 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: 'black',
     height: 300,
-    alignItems: 'center',
+    alignItems: 'flex-end',
+      '',
   },
   textOneStyle: {
     borderWidth: 3,
@@ -25,7 +26,8 @@ const styles = StyleSheet.create({
   textTwoStyle: {
     borderWidth: 3,
     borderColor: 'red',
-    alignSelf: 'stretch',
+    position: 'absolute',
+    fontSize: 18,
   },
   textThreeStyle: {
     borderWidth: 3,
