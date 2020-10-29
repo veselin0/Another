e6c3d32968f24c64cf13693a33c67ec63e155a5c
Veselin <vesselin60@gmail.com>
2020-10-29 10:38:59 +0200

    74. Align Self on Children

--- screens/BoxScreen.js
+++ screens/BoxScreen.js
@@ -16,20 +16,19 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: 'black',
     height: 300,
+    alignItems: 'center',
   },
   textOneStyle: {
     borderWidth: 3,
     borderColor: 'red',
-    flex: 4,
   },
   textTwoStyle: {
     borderWidth: 3,
     borderColor: 'red',
-    flex: 4,
+    alignSelf: 'stretch',
   },
   textThreeStyle: {
     borderWidth: 3,
     borderColor: 'red',
-    flex: 2,
   },
 });
